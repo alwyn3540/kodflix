b6e4fd31069ce896020458a50c43dd70200a2b80
Michael Gill <michaelgill@Omaewamhinderiu.localdomain>
2019-01-07 16:42:51 +0000

    add film poster #1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import  movieposter from './oldtimeimage.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Netflix</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <img src ={movieposter} alt = 'old film poster'/>
       </div>
     );
   }
