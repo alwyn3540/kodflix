1de63b5a2a3d60b55b56f5cb9f4d8133b29e2da7
Michael Gill <michaelgill@Omaewamhinderiu.localdomain>
2019-01-22 13:16:52 +0000

    add 'back to home page link' # 8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Gallery from './Gallery';
+import Details from './Details' ;
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
@@ -10,20 +12,11 @@ class App extends Component {
         <div className="App">
           <Route exact path='/' component={Gallery} />
           <Route exact path='/:details' component={Details} />
-          
         </div>
       </Router>
 
     );
   }
 }
-function Details() {
-   return (
-     <div><h4>Hello, this will be the details page for each Movie & TV show </h4></div>
-   
-   )
-}
-
-
 
 export default App;
