ddbfd82db7608bb7f8f59788a539b2d1534a40ea
Michael Gill <michaelgill@Omaewamhinderiu.localdomain>
2019-01-24 16:03:56 +0000

    amend # 11

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TVShow    from  './TVShow';
+import TVShow from './TVShow';
 import tvPoster1 from './images/tvposter1.jpeg';
 import tvPoster2 from './images/tvposter2.jpeg';
 import tvPoster3 from './images/tvposter3.jpeg';
@@ -14,16 +14,27 @@ export default function Gallery() {
   return (
     <div>
       <div className='crate'>
-        <TVShow id='sideswiped' name ='sideswiped' image={tvPoster1} />
-        <TVShow id= 'women-on-the-verge' name='Women on The Verge' image={tvPoster2} />
-        <TVShow id='the-tracey-morgan-show' name= 'The Tracey Morgan Show' image={tvPoster3} />
+        <TVShow id='sideswiped' name='sideswiped' image={tvPoster1} />
+        <TVShow id='women-on-the-verge' name='Women on The Verge' image={tvPoster2} />
+        <TVShow id='the-tracey-morgan-show' name='The Tracey Morgan Show' image={tvPoster3} />
+        <TVShow id='the-men-who-built-america' name='The Men who Built America' image={tvPoster4} />
+        <TVShow id='atlanta' name='Atlanta' image={tvPoster5} />
+        <TVShow id='out-of-this-world' name='Out of This World' image={tvPoster6} />
       </div>
-      <div className='crate'>
-        <TVShow id= 'the-men-who-built-america' name='The Men who Built America' image={tvPoster4}/>
-        <TVShow id= 'atlanta' name='Atlanta' image={tvPoster5} />
-        <TVShow id= 'out-of-this-world' name='Out of This World' image={tvPoster6} />
     </div>
-  </div>
 
   );
+}
+
+function newGallery() {
+  return [
+    { id: 'sideswiped', name: 'Sideswiped', image: tvPoster1 },
+    { id: 'women on the verge', name: 'Women on the Verge', image: tvPoster2 },
+    { id: 'the tracey morgan show', name: 'The Tracey Morgan Show', image: tvPoster3 },
+    { id: 'the men who built america', name: 'The Men Who Built America', image: tvPoster4 },
+    { id: 'atlanta', name: 'Atlanta', image: tvPoster5 },
+    { id: 'out of this world', name: 'Out of this World', image: tvPoster6 }
+
+  ];
+
 }
