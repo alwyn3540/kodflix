63f8bc0c56b76da7316866678ca05ebfeee37fc3
Michael Gill <michaelgill@Omaewamhinderiu.localdomain>
2019-01-28 16:25:39 +0000

    #13 IntroduceState

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-export default function Details() {
-    return (
-        <div>
-            <h4>Hello, this will be the details page for each Movie & TV show </h4>
-            <Link to='/'>Back to Home Page</Link>
-        </div>
 
-    );
+
+export default class Details extends React.Component{
+
+    constructor() {
+        super();
+        this.state = {
+            detailsPage: 'Hello, this will be the details page for each Movie & TV show '    
+            };
+        }
+
+        componentDidMount() {
+            setTimeout (() => {
+                this.setState ( {
+                    detailsPage: 'Great TV shows on Kodflix!'
+                });
+            }, 2000);
+        }
+
+
+    render () {
+        return (
+            <div>
+                <h4>{this.state.detailsPage}</h4>
+                <Link to='/'>Back to Home Page</Link>
+            </div>
+    
+        );
+    }
 }
