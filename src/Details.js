d517b7687b9a0266d7d1e5d652328d301d348020
Michael Gill <michaelgill@Omaewamhinderiu.localdomain>
2019-01-29 12:14:45 +0000

    #13 pull request amendments

--- src/Details.js
+++ src/Details.js
@@ -14,19 +14,16 @@ export default class Details extends React.Component{
         componentDidMount() {
             setTimeout (() => {
                 this.setState ( {
-                    detailsPage: 'Great TV shows on Kodflix!'
+                    detailsPage:'Great TV shows on Kodflix!'
                 });
             }, 2000);
         }
-
-
     render () {
         return (
             <div>
                 <h4>{this.state.detailsPage}</h4>
                 <Link to='/'>Back to Home Page</Link>
             </div>
-    
         );
     }
 }
--- src/newGallery.js
+++ src/newGallery.js
@@ -8,11 +8,11 @@ import tvPoster6 from './images/tvposter6.jpeg';
 export default function newGallery() {
   return [
     { id: 'sideswiped', name: 'Sideswiped', image: tvPoster1 },
-    { id: 'women on the verge', name: 'Women on the Verge', image: tvPoster2 },
-    { id: 'the tracey morgan show', name: 'The Tracey Morgan Show', image: tvPoster3 },
-    { id: 'the men who built america', name: 'The Men Who Built America', image: tvPoster4 },
+    { id: 'women-on-the-verge', name: 'Women on the Verge', image: tvPoster2 },
+    { id: 'the-tracey-morgan-show', name: 'The Tracey Morgan Show', image: tvPoster3 },
+    { id: 'the-men-who-built-america', name: 'The Men Who Built America', image: tvPoster4 },
     { id: 'atlanta', name: 'Atlanta', image: tvPoster5 },
-    { id: 'out of this world', name: 'Out of this World', image: tvPoster6 }
+    { id: 'out-of-this-world', name: 'Out of this World', image: tvPoster6 }
 
   ];
 
