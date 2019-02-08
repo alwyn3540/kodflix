a4d5454ecc42eddd3aa2fbfdc7d0255507e16929
Michael Gill <michaelgill@Omaewamhinderiu.localdomain>
2019-02-08 12:16:11 +0000

    change which side is rendered

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -30,11 +30,15 @@ export default class Details extends React.Component {
             return (
                 <div className='Details'>
                     <h4>{this.state.showDetails.name}</h4>
-                    <div className='crate'>
-                        <div>{this.state.showDetails.synopsis}</div>
-                        <img
-                            src={this.state.showDetails.image}
-                            alt={this.state.showDetails.name} />
+                    <div className='crate is-small'>
+                        <div className='crate'>
+                            <img
+                                src={this.state.showDetails.image}
+                                alt={this.state.showDetails.name} />
+                        </div>
+                        <div className='crate'>
+                            <div>{this.state.showDetails.synopsis}</div>
+                        </div>
                     </div>
                     <Link to='/'>Back to Home Page</Link>
                 </div>
